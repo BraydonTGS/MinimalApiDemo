5a3b4f73b0959fa93068059963e6ca97fb2e53c4
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-03-27 20:12:50 -0500

    Added TestBase for Seeding Mock Data

--- MinimalApi.Tests/Base/TestBase.cs
+++ MinimalApi.Tests/Base/TestBase.cs
@@ -1,6 +1,36 @@
-﻿namespace MinimalApi.Tests.Base
+﻿using Castle.Core.Smtp;
+using MinimalApi.DataAccess.Data;
+using MinimalApi.DataAccess.DbAccess;
+using MinimalApi.DataAccess.Models;
+using Moq;
+using System.Collections;
+
+namespace MinimalApi.Tests.Base
 {
     public class TestBase
     {
+        protected readonly IUserRepository _userRepository;
+        protected readonly Mock<ISqlDataAccess> _sqlDataAccess;
+        protected IEnumerable<UserDto> _users = Enumerable.Empty<UserDto>();
+
+        public TestBase()
+        {
+            _sqlDataAccess = new Mock<ISqlDataAccess>();
+            _userRepository = new UserRepository(_sqlDataAccess.Object);
+            SeedListWithMockUsers();
+        }
+
+        private void SeedListWithMockUsers()
+        {
+            _users = new List<UserDto>()
+            {
+                new UserDto() {FirstName = "Braydon", LastName = "Sutherland", UserName = "Geo", Password = "Flambe"},
+                new UserDto() {FirstName = "Colin", LastName = "Sutherland", UserName = "Colly", Password = "Goose"},
+                new UserDto() {FirstName = "Sally", LastName = "Sutherland", UserName = "Salmeaux", Password = "Cheese"},
+                new UserDto() {FirstName = "Devon", LastName = "Sutherland", UserName = "RiverRat", Password = "Rivers"},
+                new UserDto() {FirstName = "Mackenzie", LastName = "Sutherland", UserName = "MackTown", Password = "Lincoln"}
+
+            }; 
+        }
     }
 }
--- MinimalApi.Tests/DataAccess/UserRepositoryShould.cs
+++ MinimalApi.Tests/DataAccess/UserRepositoryShould.cs
@@ -1,8 +1,26 @@
-﻿using MinimalApi.Tests.Base;
+﻿using Microsoft.SqlServer.Server;
+using MinimalApi.DataAccess.Data;
+using MinimalApi.DataAccess.DbAccess;
+using MinimalApi.DataAccess.Models;
+using MinimalApi.Tests.Base;
+using Moq;
 
 namespace MinimalApi.Tests.DataAccess
 {
     public class UserRepositoryShould : TestBase
     {
+        [Fact]
+        public async Task GetAllUsers_ShouldReturnAllUsersAsync()
+        {
+            // Arrange //
+            _sqlDataAccess.Setup(x => x.LoadData<UserDto>(It.IsAny<string>(), default, "Default"))
+                .ReturnsAsync(_users);
+
+            // Act //
+            var actualUsers = await _userRepository.GetAllAsync();
+
+            // Assert //
+            Assert.NotNull(actualUsers);
+        }
     }
 }
