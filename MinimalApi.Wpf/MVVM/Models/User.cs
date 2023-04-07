0c6cf22e6b42a7d3db0fa2adddf13e3d61544aba
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-04-06 19:58:03 -0500

    added user class

--- MinimalApi.Wpf/MVVM/Models/User.cs
+++ MinimalApi.Wpf/MVVM/Models/User.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MinimalApi.Wpf.MVVM.Models
+﻿namespace MinimalApi.Wpf.MVVM.Models
 {
-    internal class User
+    public class User
     {
     }
 }
